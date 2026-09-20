20d75e34a485d84564070caf662cb9cd40e26f9c
Raydelis Hilario <raydelishilariomendez@gmail.com>
2026-09-20 18:29:44 -0400

    Agrega tarea 3 test a api jwt

---
+++ Tarea3-Test/src/controllers/v1/tareas.controller.js
@@ -0,0 +1,23 @@
+import { prisma } from "../../db.js"
+
+export const getTareasPublicas = async (req, res, next) => {
+  try {
+    const tareas = await prisma.tarea.findMany()
+    res.json(tareas)
+  } catch (err) { next(err) }
+}
+
+export const createTareaPublica = async (req, res, next) => {
+  try {
+    const { titulo, usuarioId } = req.body
+
+    if (!titulo || !usuarioId) {
+      return res.status(400).json({ error: "titulo y usuarioId son obligatorios" })
+    }
+    
+    const tarea = await prisma.tarea.create({
+      data: { titulo, usuarioId }
+    })
+    res.status(201).json(tarea)
+  } catch (err) { next(err) }
+}
